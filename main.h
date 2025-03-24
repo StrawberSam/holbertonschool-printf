71bd94e2f4bc4770771c34d8f1fcf8250c063a6b
StrawberSam <s.roche53@hotmail.com>
2025-03-24 11:16:33 +0100

    Ajout structure

--- main.h
+++ main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include "stdarg.h"
+
+typedef struct type_function
+{
+	char type;
+	void (*function_print)(va_list arguments);
+} type_function;
+
 int _putchar(char c);
 
 #endif /* MAIN_H */
