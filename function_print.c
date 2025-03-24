f6d07b3bac43dfc9d93c1c97a072d45afbbbf24c
StrawberSam <s.roche53@hotmail.com>
2025-03-24 14:13:41 +0100

    Ajout fonction print string

--- function_print.c
+++ function_print.c
@@ -2,9 +2,18 @@
 #include "stdarg.h"
 #include "stdio.h"
 
+int i;
+
 void print_char(va_list arguments)
 {
 	va_arg(arguments, char);
 	_putchar('c');
 }
 
+void print_string(va_list arguments)
+{
+	va_arg(arguments, char *);
+
+	for (i = 0; i != '\0'; i++)
+	_putchar(i);
+}
