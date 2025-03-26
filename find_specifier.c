ee238b88a3d2982745170fd005c7d5e753fa7484
Timi-Holberton <timi.holberton@gmail.com>
2025-03-27 00:07:09 +0100

    Création fichier contenant les tableau de structure et la boucle de comparaison entre format et tableau

---
+++ find_specifier.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * find_specifier - trouve le bon spécifieur avec la bonne fonction
+ * @format: Chaîne de charactère avec format
+ * @i: index du caractère après %, celui qu'on doit vérifier
+ * @args: Liste des arguments pour print la valeur à afficher
+ * @total_char: pointeur vers le nombre total de caractères compté
+ * Return: retourne Nouvel index après traitement
+ */
+int find_specifier(const char *format, int i, va_list args, int *total_char)
+{
+	int j;
+	type_function type[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percent},
+		{'\0', NULL}
+	};
+
+	for (j = 0; type[j].type_cifs != '\0'; j++)
+	{
+		if (format[i] == type[j].type_cifs)
+		{
+			*total_char += type[j].function_print(args);
+			return (i);
+		}
+	}
+	_putchar('%');
+	_putchar(format[i]);
+	*total_char += 2;
+	return (i);
+}
