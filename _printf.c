47c025eb6a87a2d6ef1ba9d58a819b4890dc714d
Timi-Holberton <timi.holberton@gmail.com>
2025-03-27 08:52:18 +0100

    ajout de l'option si rien après % ne rien print

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@ int _printf(const char *format, ...)
 	int i = 0, total_char = 0;
 	va_list args;
 
-	if (format == NULL)
+	if (format == NULL || format[i + 1] == '\0')
 		return (-1);
 
 	va_start(args, format);
